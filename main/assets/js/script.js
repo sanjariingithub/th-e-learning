6aa6211e8fd05005d2be77f2ba90a12649fe9543
dev-protim <roypranto445@gmail.com>
2022-11-24 22:31:32 +0600

    Fix navbar toggle issue.

--- main/assets/js/script.js
+++ main/assets/js/script.js
@@ -1,33 +1,33 @@
 jQuery(function ($) {
 
     'use strict';
-   // Navbar offcanvas dropdown toggle starts
-   const navbarOffCanvas = document.querySelector(".offcanvas.navbar-content__item");
-   if (navbarOffCanvas) {
-       navbarOffCanvas.addEventListener('show.bs.offcanvas', event => {
+    // Navbar offcanvas dropdown toggle starts
+    const navbarOffCanvas = document.querySelector(".offcanvas.navbar-content__item");
+    if (navbarOffCanvas) {
+        navbarOffCanvas.addEventListener('show.bs.offcanvas', event => {
            $(".offcanvas.navbar-content__item .offcanvas-body .nav-item a").on("click", function () {
-            
-               var $this = $(this);
-               if ($this.hasClass("dropdown-toggle")) {
-                   $this.toggleClass("icon-rotate");
-                   $this.next().slideToggle();
-                   if ($this.parent().siblings().children().hasClass("dropdown-toggle")) {
-                       $this.parent().siblings().children().removeClass("icon-rotate");
-                       $this.parent().siblings().children().next().slideUp();
-                   }
-               }
-           });
-       })
 
-       // When navbar offcanvas gets hide
-       navbarOffCanvas.addEventListener('hide.bs.offcanvas', event => {
-           $(".nav-item a").removeClass("icon-rotate");
-           $(".nav-item a").next().slideUp();
-       })
-   }
-   // Navbar offcanvas dropdown toggle ends
+                var $this = $(this);
+                if ($this.hasClass("dropdown-toggle")) {
+                    $this.toggleClass("icon-rotate");
+                    $this.next().slideToggle();
+                    if ($this.parent().siblings().children().hasClass("dropdown-toggle")) {
+                        $this.parent().siblings().children().removeClass("icon-rotate");
+                        $this.parent().siblings().children().next().slideUp();
+                    }
+                }
+            });
+        })
 
-   
+        // When navbar offcanvas gets hide
+        //    navbarOffCanvas.addEventListener('hide.bs.offcanvas', event => {
+        //        $(".nav-item a").removeClass("icon-rotate");
+        //        $(".nav-item a").next().slideUp();
+        //    })
+    }
+    // Navbar offcanvas dropdown toggle ends
+
+
     // Hot Deals Carousel starts
     $('.hot-deals__carousel').owlCarousel({
         loop: true,
@@ -49,18 +49,18 @@ jQuery(function ($) {
     // Hot Deals Carousel ends
 
     $('.owl-three').owlCarousel({
-        loop:false,
-        margin:10,
-        nav:true,
-        responsive:{
-            0:{
-                items:0
+        loop: false,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 0
             },
-            600:{
-                items:2
+            600: {
+                items: 2
             },
-            1000:{
-                items:4
+            1000: {
+                items: 4
             }
         }
     });
